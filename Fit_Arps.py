6e4f33d32c3e11c7dc29087f9c281a18f5a06be6
Rashmin Dandekar <rdandekar@slb.com>
2024-07-01 14:33:46 +0800

    cleanup

--- Fit_Arps.py
+++ Fit_Arps.py
@@ -15,15 +15,12 @@ dates, rates,_=arps.get_arps_dc(500,0.0005,start_date=dt.datetime(2020,1,1),end_
 times=[(dates[i]-dates[0]).days for i in range(len(dates))]
 rates=np.random.normal(loc=np.array(rates),scale=np.array(rates)*(0.25*np.random.rand()))
 
-
 popt, pcov=curve_fit(arps_for_fit,times,rates,bounds=([rates[0]*0.9,1e-8,1e-8],[rates[0]*1.1,1,1]))
-
 
 fitted=[arps_for_fit(times[i],popt[0],popt[1],popt[2]) for i in range(len(times))]
 
 outputs=[[times[i],rates[i],fitted[i]] for i in range(len(times))]
 print(tabulate(outputs))
-
 
 
 plt.plot(times,rates,'o')
